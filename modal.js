824efce0a6b64fc34089284c8f22e08293a5e77f
taki <tarassevcuk700@gmail.com>
2024-11-04 22:48:22 +0200

    update animation

--- modal.js
+++ modal.js
@@ -2,17 +2,24 @@ const modalOpenButton = document.querySelector('[data-modal-open]');
 const modalCloseButton = document.querySelector('[data-modal-close]');
 const backdrop = document.querySelector('.backdrop');
 
-modalOpenButton.addEventListener('click', () => {
+function openModal() {
     backdrop.classList.remove('is-hidden', 'is-closing');
     backdrop.classList.add('is-visible');
-});
 
-modalCloseButton.addEventListener('click', () => {
+    backdrop.addEventListener('animationend', () => {
+        backdrop.classList.add('is-active'); 
+    }, { once: true });
+}
+
+function closeModal() {
+    backdrop.classList.remove('is-active');
     backdrop.classList.add('is-closing');
 
-    // Очікуємо закінчення анімації, потім приховуємо модальне вікно
     backdrop.addEventListener('animationend', () => {
         backdrop.classList.remove('is-visible', 'is-closing');
         backdrop.classList.add('is-hidden');
     }, { once: true });
-});
+}
+
+modalOpenButton.addEventListener('click', openModal);
+modalCloseButton.addEventListener('click', closeModal);
