48d488a0e5ad08d9ce410bf7ae6e5efa1ae2e17c
taki <tarassevcuk700@gmail.com>
2024-11-04 19:30:17 +0200

    update animation

--- modal.js
+++ modal.js
@@ -1,15 +1,18 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      document.body.classList.toggle("no-scroll");
-    }
-  })();
+const modalOpenButton = document.querySelector('[data-modal-open]');
+const modalCloseButton = document.querySelector('[data-modal-close]');
+const backdrop = document.querySelector('.backdrop');
+
+modalOpenButton.addEventListener('click', () => {
+    backdrop.classList.remove('is-hidden', 'is-closing');
+    backdrop.classList.add('is-visible');
+});
+
+modalCloseButton.addEventListener('click', () => {
+    backdrop.classList.add('is-closing');
+
+    // Очікуємо закінчення анімації, потім приховуємо модальне вікно
+    backdrop.addEventListener('animationend', () => {
+        backdrop.classList.remove('is-visible', 'is-closing');
+        backdrop.classList.add('is-hidden');
+    }, { once: true });
+});
