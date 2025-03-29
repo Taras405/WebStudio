683fdeebd1c816e0372673ffd0d19dca3adf6029
taki <tarassevcuk700@gmail.com>
2025-03-30 00:06:33 +0200

    add webicon header adaptiv 320-375-425-768

--- js/menu.js
+++ js/menu.js
@@ -2,21 +2,26 @@
     const mobileMenu = document.querySelector('.js-menu-container');
     const openMenuBtn = document.querySelector('.js-open-menu');
     const closeMenuBtn = document.querySelector('.js-close-menu');
-
+  
     const toggleMenu = () => {
-        const isOpen = mobileMenu.classList.toggle('is-open');
-        openMenuBtn.setAttribute('aria-expanded', isOpen);
-        mobileMenu.setAttribute('aria-hidden', !isOpen);
+      const isOpen = mobileMenu.classList.toggle('is-open');
+      openMenuBtn.setAttribute('aria-expanded', isOpen);
+      mobileMenu.setAttribute('aria-hidden', !isOpen);
+      document.body.style.overflow = isOpen ? 'hidden' : 'auto'; // Щоб заблокувати скролл при відкритому меню
     };
-
+  
     openMenuBtn.addEventListener('click', toggleMenu);
     closeMenuBtn.addEventListener('click', toggleMenu);
-
+  
     window.addEventListener('resize', () => {
-        if (window.innerWidth >= 768) {
-            mobileMenu.classList.remove('is-open');
-            openMenuBtn.setAttribute('aria-expanded', false);
-            mobileMenu.setAttribute('aria-hidden', true);
+      if (window.innerWidth >= 768) {
+        if (mobileMenu.classList.contains('is-open')) {
+          mobileMenu.classList.remove('is-open');
+          openMenuBtn.setAttribute('aria-expanded', false);
+          mobileMenu.setAttribute('aria-hidden', true);
+          document.body.style.overflow = 'auto';
         }
+      }
     });
-})();
+  })();
+  
