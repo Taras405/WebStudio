f02d156c2071d0d580e4d29eccdda1dcd28b60c2
taki <tarassevcuk700@gmail.com>
2025-05-12 22:37:22 +0300

    modal window fix an dadaptiv  start

---
+++ js/trigger.js
@@ -0,0 +1,27 @@
+// const trigger = document.getElementById('trigger');
+// const icon = document.getElementById('hint-icon');
+
+// trigger.addEventListener('mouseenter', () => {
+//   icon.classList.add('icon-visible');
+// });
+
+// trigger.addEventListener('mouseleave', () => {
+//   icon.classList.remove('icon-visible');
+// });
+
+// const trigger = document.getElementById('trigger');
+// const image = document.getElementById('hint-img');
+
+// // Ховері
+// trigger.addEventListener('mouseenter', () => {
+//   image.classList.add('icon-visible');
+// });
+
+// trigger.addEventListener('mouseleave', () => {
+//   image.classList.remove('icon-visible');
+// });
+
+// // Клік (за бажанням, окремо або разом з ховером)
+// trigger.addEventListener('click', () => {
+//   image.classList.toggle('icon-visible');
+// });
