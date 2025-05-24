70a4a500dcbd022e5b0672a8c471bb85ac0e6cbe
taki <tarassevcuk700@gmail.com>
2025-05-24 21:11:30 +0300

    add gulp and setings

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 
 gulp.task('styles', () => {
-    return gulp.src('src/scss/**/*.scss')
+    return gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(cleanCSS())
         .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('watch', () => {
-    gulp.watch('src/scss/**/*.scss', gulp.series('styles'));
+    gulp.watch('sass/**/*.scss', gulp.series('styles'));
 });
 
 gulp.task('default', gulp.series('styles', 'watch'));
